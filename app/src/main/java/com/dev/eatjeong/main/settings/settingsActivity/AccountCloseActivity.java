b0e6e0718596682e62cd1323fe10cb57ea2b8e92
남오승 <developernam92@gmail.com>
2020-03-17 23:38:46 +0900

    회원탈퇴 추가

--- app/src/main/java/com/dev/eatjeong/main/settings/settingsActivity/AccountCloseActivity.java
+++ app/src/main/java/com/dev/eatjeong/main/settings/settingsActivity/AccountCloseActivity.java
@@ -1,19 +1,31 @@
 package com.dev.eatjeong.main.settings.settingsActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.Selection;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.fragment.app.Fragment;
 
 import com.dev.eatjeong.R;
 import com.dev.eatjeong.common.CommonMapResponseVO;
+import com.dev.eatjeong.layout.ClearEditText;
 import com.dev.eatjeong.login.LoginActivity;
+import com.dev.eatjeong.main.search.searchFragment.PlaceListFragment;
 import com.dev.eatjeong.main.settings.SettingsRetrofitAPI;
 
 import retrofit2.Call;
@@ -26,33 +38,65 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class AccountCloseActivity extends AppCompatActivity {
     private String user_id, sns_division;
     private Retrofit mRetrofit;
-
+    ProgressDialog progressDialog;
     private SettingsRetrofitAPI mSettingsRetrofitAPI;
 
     private Call<CommonMapResponseVO> mCallCommonMapResponseVO;
 
-    EditText password;
-    Button confirm;
-
+    ClearEditText password;
+    AppCompatButton confirm,cancel;
+    AppCompatTextView password_message;
+    private ConstraintLayout back_button,exit_button;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.user_account_close);
+        View action_bar = findViewById(R.id.action_bar);
+        back_button = (ConstraintLayout)action_bar.findViewById(R.id.back_button);
+        TextView back_text = action_bar.findViewById(R.id.back_text);
+        TextView textview1 = action_bar.findViewById(R.id.textview1);
+        exit_button = action_bar.findViewById(R.id.exit_button);
+        confirm = findViewById(R.id.confirm);
+        cancel = findViewById(R.id.cancel);
+        password_message = findViewById(R.id.password_message);
+        password = findViewById(R.id.password);
+        back_text.setText("뒤로가기");
+        textview1.setText("탈퇴안내");
+        exit_button.setVisibility(View.INVISIBLE);
 
+        progressDialog = new ProgressDialog(this);
         Intent intent = getIntent();
-
-        password = (EditText)findViewById(R.id.password);
-        confirm = (Button)findViewById(R.id.confirm);
 
         user_id = intent.getStringExtra("user_id");
         sns_division = intent.getStringExtra("sns_division");
 
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setRetrofitInit();
 
-                callPlaceInfoResponse();
+            }
+        });
+//
+//        confirm.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                setRetrofitInit();
+//
+//                callPlaceInfoResponse();
+//            }
+//        });
+
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
             }
         });
 
@@ -86,10 +130,15 @@ public class AccountCloseActivity extends AppCompatActivity {
 
 
         mSettingsRetrofitAPI = mRetrofit.create(SettingsRetrofitAPI.class);
-
+        callPlaceInfoResponse();
     }
 
     private void callPlaceInfoResponse() {
+
+        progressDialog.setMessage("탈퇴 처리중입니다.");
+        progressDialog.setCancelable(true);
+        progressDialog.setProgressStyle(android.R.style.Widget_ProgressBar_Horizontal);
+        progressDialog.show();
         mCallCommonMapResponseVO = mSettingsRetrofitAPI.setAccountClose(user_id,password.getText().toString());
         mCallCommonMapResponseVO.enqueue(mRetrofitCallback);
 
@@ -102,23 +151,15 @@ public class AccountCloseActivity extends AppCompatActivity {
             Log.e("dd", response.body().getMessage());
 
             if(response.body().getCode().equals("200")){
-                if(response.body().getDataList().get("result_flag").equals("true")){
-                    SharedPreferences sp = getSharedPreferences("login",MODE_PRIVATE);
-
-                    SharedPreferences.Editor editor = sp.edit();
-
-                    editor.clear();
-
-                    editor.commit();
-
-
-                    Intent login = new Intent(AccountCloseActivity.this, LoginActivity.class);
-                    login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-
-                    startActivityForResult(login,0);//액티비티 띄우기
-                    AccountCloseActivity.this.overridePendingTransition(R.anim.fadein,0);
+                if(response.body().getDataList().get("result_flag").equals("true")){ //비번 통과시
+                    Intent intent = new Intent(getApplicationContext(), AccountClosePopupActivity.class);
+                    intent.putExtra("data", "Test Popup");
+                    startActivityForResult(intent, 1);
+                }else{
+                    password_message.setVisibility(View.VISIBLE);
                 }
             }
+            progressDialog.dismiss();
         }
 
 
@@ -128,6 +169,7 @@ public class AccountCloseActivity extends AppCompatActivity {
 
             Log.e("ss", "asdasdasd");
             t.printStackTrace();
+            progressDialog.dismiss();
 
         }
     };
@@ -142,5 +184,30 @@ public class AccountCloseActivity extends AppCompatActivity {
         super.onBackPressed();
 
 
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1){
+            if(resultCode == 1){
+                SharedPreferences sp = getSharedPreferences("login",MODE_PRIVATE);
+
+                SharedPreferences.Editor editor = sp.edit();
+
+                editor.clear();
+
+                editor.commit();
+
+
+                //로그인 화면으로 돌아가면서 기존 액티비티 스택 다 지움 (생명주기 영향x)
+                Intent login = new Intent(AccountCloseActivity.this, LoginActivity.class);
+                login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                startActivity(login);//액티비티 띄우기
+                AccountCloseActivity.this.overridePendingTransition(R.anim.slide_in_left,0);
+            }
+
+        }
     }
 }
---
+++ app/src/main/java/com/dev/eatjeong/main/settings/settingsActivity/AccountClosePopupActivity.java
@@ -0,0 +1,56 @@
+package com.dev.eatjeong.main.settings.settingsActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+import com.dev.eatjeong.R;
+
+
+public class AccountClosePopupActivity extends Activity {
+    TextView txtText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //타이틀바 없애기
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.account_close_popup_activity);
+
+        //UI 객체생성
+        txtText = (TextView)findViewById(R.id.txtText);
+
+    }
+
+    //확인 버튼 클릭
+    public void mOnClose(View v){
+        //데이터 전달하기
+        Intent intent = new Intent();
+        intent.putExtra("result", "Close Popup");
+        setResult(1, intent);
+
+        //액티비티(팝업) 닫기
+        overridePendingTransition(R.anim.fadeout, R.anim.stay);
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //바깥레이어 클릭시 안닫히게
+        if(event.getAction()== MotionEvent.ACTION_OUTSIDE){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        //안드로이드 백버튼 막기
+        return;
+    }
+}
